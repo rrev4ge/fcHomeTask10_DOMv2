3b3fcb1ddc280d9f97b16b295f29a1eafdf2411a
rrev <68103311+rrev4ge@users.noreply.github.com>
2020-09-19 14:44:30 +0300

    compleate 3rd task

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,12 +30,48 @@ function setClass(elementNode, attribute) {
     removeElementAttributesValue(elementNode, attribute);
   }
   if (!checkAttribute (elementNode, attribute)) {
-    setElementAttributes(elementNode, attribute);
-    
+    setElementAttributes(elementNode, attribute); 
   }
-  
 }
 
 
 
+const objArray = [
+  {id: "a", title: "Object A", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ullam vitae unde, voluptatibus."},
+  {id: "b", title: "Object B", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ullam vitae unde, voluptatibus."},
+  {id: "c", title: "Object C", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ullam vitae unde, voluptatibus."},
+  {id: "d", title: "Object D", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ullam vitae unde, voluptatibus."},
+];
+
+
+function addLi(objArr) {
+  for (const obj of objArr) {
+    const {
+      id,
+      title,
+      description,
+    } = obj;
+    const delBtn = createDomElement("button", {class: "button",}, [document.createTextNode("Delete")]);
+    delBtn.onclick = removeParrent;
+    const content = title + " " + description;
+    const li = createDomElement("li", {id: id,}, [document.createTextNode(content), delBtn]);
+    li.addEventListener("click", (event) => {
+      const {
+        target
+      } = event;
+      target.classList.add("www");
+    })   
+    ul.append(li);
+  }
+  
+}
+
+addLi(objArray);
+
+function removeParrent (event) {
+    const {
+      target
+    } = event;
+    target.parentElement.remove();
+}
 
